1fc1e5caa8b6d4ce61593425c90989b24a4efb7b
Mohammed Hameed <mohammed.hameed@email.com>
2018-02-24 18:55:44 -0600

    added data.js, added append functionality

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,37 @@
-var reguestQuestions;
+$(document).ready(function(){
+    
+console.log (myQuestions);
+// iterate through the length of the array myQuestions and create P element
+for (var i = 0; i < myQuestions.length; i++){
+    var newP = $("<p class ='lead '>" + myQuestions[i].question + "</p>");
+    
+    $("#questions").append(newP);
 
-reguestQuestions = new XMLHttpRequest();
-reguestQuestions.open("GET", "./js/questions.json", true);
-reguestQuestions.onload = function() {
-    console.log (reguestQuestions.responseText);
-    console.log (reguestQuestions.status)
-}
-reguestQuestions.send(null);
+    //create a box for the radio buttons
+    var row = $("<div class = 'row "+myQuestions[i].name +"' </div>")
+    var hairLine = $("<hr>")
+    $("#questions").append(row);
+    $("#questions").append(hairLine);
+
+    for (var ai = 0; ai < myQuestions[i].answers.length; ai++){
+        
+
+        var choice = $("<div>");
+            choice.addClass ("col-sm-3 txtred");
+            choice.html("<input type = 'radio' name = '"+myQuestions[i].name+"' value = '" + myQuestions[i].answers[ai] + "'/>"
+             + myQuestions[i].answers[ai])
+        $("."+myQuestions[i].name).append(choice);
+        
+    } //end of answers loop
+    
+} // end of questions loop
+
+var answerArray
+$("input").on("change", function(){
+    answerArray = $(this).val();
+    console.log(answerArray)
+})
+
+
+
+})
