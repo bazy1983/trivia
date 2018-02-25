585829626142e56360ddf29e73cfe3bf94c65b37
Mohammed Hameed <mohammed.hameed@email.com>
2018-02-24 21:49:53 -0600

    added counter functionality,countdown, and visual effect effects to HTML

--- js/data.js
+++ js/data.js
@@ -3,60 +3,60 @@ var myQuestions = [
         question : "1. Not a marvel superhero",
         answers: ["Thor", "Daredevil", "Black Widow", "The flash"],
         correct: "The flash",
-        name : 1
+        name : 0
     },
     {
         question : "2. Do the math, 1000 + 10 + 1000 + 20 + 1000 + 30 + 1000 + 40",
         answers: ["4010", "4100", "4200", "5000"],
         correct: "4100",
-        name : 2
+        name : 1
     },
     {
         question : "3. What is the color of the stars in the flag of the United States of America?",
         answers: ["Red", "Blue", "White", "Black"],
         correct: "White",
-        name : 3
+        name : 2
     },
     {
         question : "4. What's the total number of dots on a pair of dice?",
         answers: ["41", "42", "43", "44"],
         correct: "42",
-        name : 4
+        name : 3
     },
     {
         question : "5.Traditionally, how many Wonders of the World are there?",
         answers: ["6", "7", "8", "9"],
         correct: "7",
-        name : 5
+        name : 4
     },
     {
         question : "6. What is the chemical symbol for Hydrogen?",
         answers: ["H", "He", "Hy", "Hg"],
         correct: "H",
-        name : 6
+        name : 5
     },
     {
         question : "7. How many sides does an octagon have?",
         answers: ["Six", "Seven", "Eight", "nine"],
         correct: "Eight",
-        name : 7
+        name : 6
     },
     {
         question : "8.Which part of the body would a chiropodist treat?",
         answers: ["Hands", "Head", "Feet", "Back"],
         correct: "Feet",
-        name : 8
+        name : 7
     },
     {
         question : "9. What is the name of the city where the cartoon family The Simpsons live?",
         answers: ["Hollywood", "Scottsdale", "Summerfield", "Springfield"],
         correct: "Springfield",
-        name : 9
+        name : 8
     },
     {
         question : "10. Which U.S. president appears on the front of the $2 bill?",
         answers: ["Thomas Jefferson", "Abraham Lincoln", "George Washington", "Benjamin Franklin"],
         correct: "Thomas Jefferson",
-        name : 10
+        name : 9
     }
 ]
--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    
+    var correct = 0,
+        wrong = 0,
+        unanswered = 0;
+
+
     console.log (myQuestions);
     // iterate through the length of the array myQuestions and create P element
     for (var i = 0; i < myQuestions.length; i++){
@@ -27,12 +31,46 @@ $(document).ready(function(){
     } // end of questions loop
 
     var answerArray = [];
+    for (var i = 0; i < myQuestions.length; i++){
+        answerArray[i] = "";
+    };
+
     $("input").on("change", function(){
         // store clicked on values to the array
         answerArray[$(this).attr("name")] = $(this).val(); 
-        //console.log (answerArray)
+        console.log (answerArray)
     })
 
+    var timer = myQuestions.length * 3 // total time 3 seconds for each questions
+
+    var countdown = setInterval (function(){
+        $(".countdown").html(timer);
+        
+        timer--;
+        
+        if (timer === 0) {
+            clearInterval(countdown);
+            $(".timer").fadeOut();
+            for (var i = 0; i < myQuestions.length; i++){
+                if (answerArray[i] == ""){
+                    unanswered++;
+                } else if (myQuestions[i].correct == answerArray[i]){
+                    correct ++;
+                } else {
+                    wrong ++;
+                };
+                //pass values to the results div
+                $("#correct span").text (correct);
+                $("#incorrect span").text (wrong);
+                $("#unanswered span").text(unanswered);
+                //show screen div
+                $(".screen").fadeIn();
+                $(".results").addClass("animate")
+            };
+            console.log ("correct: "+ correct + "wrong: "+ wrong + "unanswered: " + unanswered)
 
 
-})
+        };
+    }, 1000);
+
+});
