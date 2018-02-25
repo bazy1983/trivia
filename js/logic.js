660aa67d6b7101c6643ad910fd36253156a6ef78
Mohammed Hameed <mohammed.hameed@email.com>
2018-02-24 19:14:12 -0600

    added on change event to the radio button and store values to array

--- js/logic.js
+++ js/logic.js
@@ -1,36 +1,37 @@
 $(document).ready(function(){
     
-console.log (myQuestions);
-// iterate through the length of the array myQuestions and create P element
-for (var i = 0; i < myQuestions.length; i++){
-    var newP = $("<p class ='lead '>" + myQuestions[i].question + "</p>");
-    
-    $("#questions").append(newP);
-
-    //create a box for the radio buttons
-    var row = $("<div class = 'row "+myQuestions[i].name +"' </div>")
-    var hairLine = $("<hr>")
-    $("#questions").append(row);
-    $("#questions").append(hairLine);
-
-    for (var ai = 0; ai < myQuestions[i].answers.length; ai++){
+    console.log (myQuestions);
+    // iterate through the length of the array myQuestions and create P element
+    for (var i = 0; i < myQuestions.length; i++){
+        var newP = $("<p class ='lead '>" + myQuestions[i].question + "</p>");
         
+        $("#questions").append(newP);
 
-        var choice = $("<div>");
-            choice.addClass ("col-sm-3 txtred");
-            choice.html("<input type = 'radio' name = '"+myQuestions[i].name+"' value = '" + myQuestions[i].answers[ai] + "'/>"
-             + myQuestions[i].answers[ai])
-        $("."+myQuestions[i].name).append(choice);
+        //create a box for the radio buttons
+        var row = $("<div class = 'row "+myQuestions[i].name +"' </div>")
+        var hairLine = $("<hr>")
+        $("#questions").append(row);
+        $("#questions").append(hairLine);
+
+        for (var ai = 0; ai < myQuestions[i].answers.length; ai++){
+            
+            // will create div, input, and text as answers to each question
+            var choice = $("<div>");
+                choice.addClass ("col-sm-3 txtred");
+                choice.html("<input type = 'radio' name = '"+myQuestions[i].name+"' value = '" + myQuestions[i].answers[ai] + "'/>"
+                + myQuestions[i].answers[ai])
+            $("."+myQuestions[i].name).append(choice);
+            
+        } //end of answers loop
         
-    } //end of answers loop
-    
-} // end of questions loop
+    } // end of questions loop
 
-var answerArray
-$("input").on("change", function(){
-    answerArray = $(this).val();
-    console.log(answerArray)
-})
+    var answerArray = [];
+    $("input").on("change", function(){
+        // store clicked on values to the array
+        answerArray[$(this).attr("name")] = $(this).val(); 
+        //console.log (answerArray)
+    })
 
 
 
